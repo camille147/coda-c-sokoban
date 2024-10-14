ad3b32deb63ccb2a96338c2290eb9d86fefe73ea
camille147 <camille.pinault@coda-student.school>
2024-10-14 09:16:54 +0200

    sokoban

--- move_u_b_h.c
+++ move_u_b_h.c
@@ -10,19 +10,19 @@ void move_use_box_h(char **board, char dir, positio *p, char user)
                 board[p->use_i][p->use_j]=user;
                 board[p->box_i][p->box_j]='X';
         }
-        else if (((p->box_i)-2)==0)
-        {
-                printf("C'est perdu !\n");
-                p->stop=(p->stop)+1;
-                exit (0);
-        }
-        else if (board[(p->box_i)-1][p->box_j]=='.')
+        else if (((p->box_i)-1)==p->vic_i && p->box_j==p->vic_j)
         {
                 printf("C'est gagné !\n");
                 p->stop=(p->stop)+1;
                 exit (0);
         }
-        else
+	else if (((p->box_i)-2)==0)
+        {
+                printf("C'est perdu !\n");
+                p->stop=(p->stop)+1;
+                exit (0);
+        }
+	else
         {
                 board[p->use_i][p->use_j]=' ';
                 p->use_i= (p->use_i) -1;
